5122178fa1fb328daa8bd30f748f9722a5a3e31f
Philipp Tiefenbacher <wizards23+bitbucket@gmail.com>
2011-09-08 02:04:11 +0200

    hacked together initial candidates, woks very well

--- src/lines.js
+++ src/lines.js
@@ -64,3 +64,159 @@ function normalizeLine(line)
 		return {x:line.x, y:line.y, dx:line.dx, dy:line.dy};
 	}
 }
+
+function intersectLinePoint(point, alpha, d, mask, fn) {
+	var tx = point.x + Math.cos(alpha)*d;
+	var ty = point.y + Math.sin(alpha)*d;
+
+
+	var xSum = 0, ySum = 0, sumN = 0;
+	var activated = 0, dead= false;	
+
+	lineDirectedIterator(Math.round(point.x), Math.round(point.y), Math.round(tx), Math.round(ty), function (x, y) {
+		if (!dead && x >= 0 && y >= 0 && x < mask.width && y < mask.height) {
+			var idx = x+mask.width*y;
+			if (fn(mask.data[idx], x, y)) {
+				xSum += x;
+				ySum += y;
+				sumN++;
+				activated = 5;
+			}
+			else if (activated) {
+				activated--;
+				if (!activated)
+					dead = true;
+			}
+		}
+	}); 
+
+	if (sumN > 0) {
+		return {x:xSum/sumN, y:ySum/sumN};
+	}
+	return {x:0, y:0};	
+}
+
+function drawLine2(p1, p2, pixels, r, g, b) {
+	drawLine(p1.x, p1.y, p2.x, p2.y, pixels, r, g, b);
+}
+
+function drawLine(x1, y1, x2, y2, pixels, r, g, b) {
+	var w = pixels.width;
+
+	xiaolinWuLineIterator(x1, y1, x2, y2, function(x, y, c) { 
+				pixels.data[4*(x+w*y)] = (1-c) * pixels.data[4*(x+w*y)] + c * r;
+				pixels.data[4*(x+w*y)+1] = (1-c) * pixels.data[4*(x+w*y)+1] + c * g;
+				pixels.data[4*(x+w*y)+2] = (1-c) * pixels.data[4*(x+w*y)+2] + c *  b;
+				//pixels.data[4*(x+w*y)+3] = 255;
+			});
+
+}
+
+function drawLine2A(p1, p2, pixels, v) {
+	drawLineA(p1.x, p1.y, p2.x, p2.y, pixels, v);
+}
+
+function drawLineA(x1, y1, x2, y2, pixels, v) {
+	var w = pixels.width;
+
+	xiaolinWuLineIterator(x1, y1, x2, y2, function(x, y, c) { 
+				pixels.data[(x+w*y)] = (1-c) * pixels.data[4*(x+w*y)] + c * v;
+			});
+}
+
+function averageLine2A(p1, p2, pixels) {
+	return averageLineA(p1.x, p1.y, p2.x, p2.y, pixels);
+}
+
+function averageLineA(x1, y1, x2, y2, pixels) {
+	var w = pixels.width;
+	var sum = 0;
+	var weightSum = 0;
+
+	xiaolinWuLineIterator(x1, y1, x2, y2, function(x, y, c) { 
+				sum += c * pixels.data[(x+w*y)];
+				weightSum += c;
+			});
+	if (weightSum == 0) return 0;
+
+	return sum/weightSum;
+}
+
+function findLineCandidates(pixels, maskPixels, bwPixelsSobolev) {
+	applyMaskAlphaOnPixels(bwPixelsSobolev, maskPixels, bwPixelsSobolev);
+	var point = calculateCenterPoint(maskPixels);
+	var d = 4*Math.sqrt(point.n);
+
+	var bestGroup = [];
+	var bestAvg = -1;
+		
+	for (var offset = 0; offset < Math.PI/2; offset += 0.1) {	
+
+		var group = [];
+		var minAvg = -1;
+		for (var i = 0; i < 4; i++) {
+			var p1 = intersectLinePoint(point, offset + i * Math.PI/2, d, bwPixelsSobolev, function(v) {return v > 40});
+			var p2 = intersectLinePoint(point, offset + i * Math.PI/2 + Math.PI/6, d, bwPixelsSobolev, function(v) {return v > 40});
+		
+			var a = Math.atan2(p1.y - p2.y, p1.x - p2.x);
+
+			var isOut = 0, isDead = false;
+			var pp1 = myLineExtender(p1, a, d, bwPixelsSobolev); 
+		
+			isOut = 0; isDead = false;
+			var pp2 = myLineExtender(p2, a + Math.PI, d, bwPixelsSobolev); 
+
+			var avg = averageLine2A(pp1, pp2, bwPixelsSobolev);
+			if (minAvg < 0 || avg < minAvg) minAvg = avg; 
+
+			group.push({p1:pp1, p2:pp2});
+
+			//drawLine2(point, p1, pixels, 255, 155, 0);
+			//drawLine2(point, p2, pixels, 255, 155, 0);
+			//drawLine2(pp1, pp2, pixels, 0, 255, 0);
+		}
+		if (bestAvg < minAvg) {
+			bestGroup = group;
+			bestAvg = minAvg;
+		}
+	}
+
+	return bestGroup;
+}
+
+function myLineExtender(point, alpha, dist, bwPixelsSobolev) {
+	var isOut = 0, isDead = false;
+	var lastX = 0, lastY = 0;
+
+	intersectLinePoint(point, alpha, dist, bwPixelsSobolev, 
+		function(v, x, y) {
+			if (isDead) return false;
+
+			if (v > 40)
+			{
+				lastX = x;
+				lastY = y
+				isOut = 0;
+			}
+			else if (isOut == 0)
+				isOut = 20;
+
+			if (isOut) { 
+				isOut--; 
+				if (!isOut) {
+					isDead = true; 
+					return true;
+				} 
+			} 
+
+			return false;
+		}
+	);
+
+	return {x:lastX, y:lastY};
+}
+
+
+	
+
+
